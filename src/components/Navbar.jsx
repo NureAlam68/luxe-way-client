b211c53be48be1a1df2a71b24a074cc9ed013d68
NureAlam68 <nurealam151068@gmail.com>
2024-12-22 20:51:37 +0600

    project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="text-slate-800 text-5xl mt-9">
+            Navbar
+            
+        </div>
+    );
+};
+
+export default Navbar;
